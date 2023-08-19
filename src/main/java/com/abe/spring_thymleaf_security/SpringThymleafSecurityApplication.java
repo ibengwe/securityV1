281d37074dec86a3c01e238bccaf6834c6457e91
=khalfankhamis <dukaniabe@gmail.com>
2023-08-19 18:14:55 +0300

    security example

---
+++ src/main/java/com/abe/spring_thymleaf_security/SpringThymleafSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.abe.spring_thymleaf_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymleafSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymleafSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abe/spring_thymleaf_security/SpringThymleafSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abe.spring_thymleaf_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymleafSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
